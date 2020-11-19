6aea28ec1e9f6e37d722304237be33ec413ba094
Uttam <uttam.laila@gmail.com>
2020-11-19 09:23:05 +0530

    Writing TDD
    only one widget tested

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,17 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:online_service_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Test for UI', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Verify that landed on HomePage.
+    expect(find.byIcon(Icons.home), findsOneWidget);
+    // tap on home button should go to swiggyPage
+    await tester.tap(find.byIcon(Icons.home));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that we are on swiggyPage.
+    expect(find.byKey(ValueKey("SwiggyPage")), findsOneWidget);
   });
 }
